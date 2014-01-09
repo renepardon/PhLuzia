d01086708b07d74f6ad3faea51c215f4be359368
Christoph, René Pardon <rene.pardon@check24.de>
2014-01-09 10:03:11 +0100

    Fixed path to module configuration

--- src/PhMagick/Module.php
+++ src/PhMagick/Module.php
@@ -2,10 +2,16 @@
 
 namespace PhMagick;
 
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
-class Module
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface
 {
     public function onBootstrap(MvcEvent $e)
     {
@@ -14,17 +20,23 @@ class Module
         $moduleRouteListener->attach($eventManager);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+            AutoloaderFactory::STANDARD_AUTOLOADER => array(
+                StandardAutoloader::LOAD_NS => array(
+                    __NAMESPACE__ => __DIR__, // . '/src/' . __NAMESPACE__,
                 ),
             ),
         );
