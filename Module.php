59a342b18dc6ad08cf55a0dc6c7ccfd0fc1e9c3b
Christoph, René Pardon <rene.pardon@check24.de>
2014-01-09 09:48:51 +0100

    Excluded PHP5.3.3 from Travis build because of openssl error; Added correct module setup for PSR-0 AND ZF2 compatibility

--- Module.php
+++ Module.php
@@ -1,32 +1,7 @@
 <?php
-
-namespace PhMagick;
-
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+/**
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/PhMagick' in order to respect PSR-0
+ */
+require_once __DIR__ . '/src/PhMagick/Module.php';
